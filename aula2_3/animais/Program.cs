7b1a8cd4182ac645a6b04e5e31f8305eae782cc5
I-NOZex <simplesmente_moi@hotmail.com>
2014-06-11 19:28:42 +0100

    Otimizaçao de codigo

--- aula2_3/animais/Program.cs
+++ aula2_3/animais/Program.cs
@@ -6,9 +6,7 @@ using System.Threading.Tasks;
 
 namespace animais {
     class Program {
-        static Mamifero m;
-        static Reptil r;
-        static Ave a; 
+        static Animal animal;
         static void Main(string[] args) {
             bool notexit = false;
             int opt = 0;
@@ -17,44 +15,31 @@ namespace animais {
             Console.WriteLine("1 - Mamifero");
             Console.WriteLine("2 - Reptil");
             Console.WriteLine("3 - Ave");
-            Console.WriteLine("0 - Sair");
+            Console.WriteLine("0 - Sair.");
             opt = int.Parse(Console.ReadLine());
             int peso = 0;
             int altura = 0;
             int nfome = 50;
             string som = "";
+            Console.WriteLine("Quanto pesa o seu bicho?");
+            peso = int.Parse(Console.ReadLine());
+            Console.WriteLine("Quanto mede o seu bicho?");
+            altura = int.Parse(Console.ReadLine());
+            Console.WriteLine("Qual o som que o seu bicho faz?");
+            som = Console.ReadLine();
+
             switch (opt) {
                 case 0:
                     Environment.Exit(0);
                     break;
                 case 1:
-                    Console.WriteLine("Quanto pesa o seu bicho?");
-                    peso = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Quanto mede o seu bicho?");
-                    altura = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Qual o som que o seu bicho faz?");
-                    som = Console.ReadLine();
-                    m = new Mamifero(peso, altura, nfome, som);
+                    animal = new Mamifero(peso, altura, nfome, som);
                     break;
-
                 case 2:
-                    Console.WriteLine("Quanto pesa o seu bicho?");
-                    peso = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Quanto mede o seu bicho?");
-                    altura = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Qual o som que o seu bicho faz?");
-                    som = Console.ReadLine();
-                    r = new Reptil(peso, altura, nfome, som);
+                    animal = new Reptil(peso, altura, nfome, som);
                     break;
-
                 case 3:
-                    Console.WriteLine("Quanto pesa o seu bicho?");
-                    peso = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Quanto mede o seu bicho?");
-                    altura = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Qual o som que o seu bicho faz?");
-                    som = Console.ReadLine();
-                    a = new Ave(peso, altura, nfome, som);
+                    animal = new Ave(peso, altura, nfome, som);
                     break;
                 default:
                     Console.WriteLine("Opção inválida, tente de novo");
@@ -76,54 +61,22 @@ namespace animais {
                         notexit = true;
                         break;
                     case 1:
-                        if (m != null) {
-                            m.alimentar();
-                            Console.WriteLine("Nivel de fome igual a {0}", m.showNivelFome());
-                        }
-                        else if (r != null) {
-                            r.alimentar();
-                            Console.WriteLine("Nivel de fome igual a {0}", r.showNivelFome());
-                        }
-                        else if (a != null) {
-                            a.alimentar();
-                            Console.WriteLine("Nivel de fome igual a {0}", a.showNivelFome());
-                        }
+                            animal.alimentar();
                         break;
                     case 2:
-                        if (m != null) {
-                            m.dormir();
-                            Console.WriteLine("Nivel de fome igual a {0}", m.showNivelFome());
-                        }
-                        else if (r != null) {
-                            r.dormir();
-                            Console.WriteLine("Nivel de fome igual a {0}", r.showNivelFome());
-                        }
-                        else if (a != null) {
-                            a.dormir();
-                            Console.WriteLine("Nivel de fome igual a {0}", a.showNivelFome());
-                        }
+                            animal.dormir();
                         break;
                     case 3:
-                        if (m != null) {
-                            m.mover();
-                            Console.WriteLine("Nivel de fome igual a {0}", m.showNivelFome());
-                        }
-                        else if (r != null) {
-                            r.mover();
-                            Console.WriteLine("Nivel de fome igual a {0}", r.showNivelFome());
-                        }
-                        else if (a != null) {
-                            a.mover();
-                            Console.WriteLine("Nivel de fome igual a {0}", a.showNivelFome());
-                        }
+                            animal.mover();
                         break;
                     default:
                         Console.WriteLine("Opção inválida, tente de novo");
                         break;
                 }
-
+                Console.WriteLine("Nivel de fome igual a {0}", animal.showNivelFome());
                 Console.WriteLine("-------------------------------------------------");
             } while (notexit == false);
+            
         }
     }
 }
