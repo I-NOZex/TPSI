ca8aba2dea227148b07a8e1c51b5b1709c80e196
I-NOZex <simplesmente_moi@hotmail.com>
2014-06-23 21:37:32 +0100

    exrs 1 e 2

--- aula 11_12/console/Program.cs
+++ aula 11_12/console/Program.cs
@@ -7,6 +7,32 @@ using System.Threading.Tasks;
 namespace console {
     class Program {
         static void Main(string[] args) {
+            /*
+            Console.WriteLine("Calculadora simples: divisão"); 
+            Console.WriteLine("****************************"); 
+            Console.Write("Insira o numerador:"); 
+            int numerador = Convert.ToInt32(Console.ReadLine()); 
+            Console.Write("Insira o denominador:"); 
+            int denominador = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine("numerador/denominador = {0}/{1} = {2}", numerador, denominador, numerador / denominador);
+            */
+            //ex1 ponto 8
+            try { 
+                Console.WriteLine("Calculadora simples: divisão"); 
+                Console.WriteLine("****************************"); 
+                Console.Write("Insira o numerador:"); 
+                int numerador = Convert.ToInt32(Console.ReadLine()); 
+                Console.Write("Insira o denominador:"); 
+                int denominador = Convert.ToInt32(Console.ReadLine()); 
+                Console.WriteLine("numerador/denominador = {0}/{1} = {2}", numerador, denominador, numerador / denominador); 
+            }
+            catch (FormatException) { 
+                Console.WriteLine("Deve inserir um número válido."); 
+            }
+            catch(DivideByZeroException) {
+                Console.WriteLine("Divisão por zero. Escolha outro denominador.");
+            }
         }
+
     }
 }
